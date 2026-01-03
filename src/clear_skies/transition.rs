40eb49c2d8664e4d114fca2c454f8a69787d4997
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-01-03 16:28:28 -0700

    feat: remove lighting and rotate cubes to show baked light

--- src/clear_skies/transition.rs
+++ src/clear_skies/transition.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::PI;
+
 use bevy::prelude::*;
 use bevy_asset_loader::asset_collection::AssetCollection;
 use bevy_pipe_affect::prelude::*;
@@ -5,7 +7,6 @@ use thiserror::Error;
 
 use crate::clear_skies::ClearSkiesState;
 use crate::clear_skies::paint_skies::Paintable;
-use crate::clear_skies::render_layers::PAINTABLE_LAYER;
 
 #[derive(Debug, Default, Copy, Clone, PartialEq, Eq, Error)]
 #[error("GLTF assets handles should be strong paths")]
@@ -14,23 +15,21 @@ pub struct GltfAssetNotStrongPath;
 pub fn spawn_scene(
     assets: Res<ClearSkiesAssetCollection>,
 ) -> Result<impl Effect + use<>, GltfAssetNotStrongPath> {
-    Ok((
-        asset_server_load_and(
-            GltfAssetLabel::Scene(0).from_asset(assets.cube.path().ok_or(GltfAssetNotStrongPath)?),
-            |handle| {
-                (0..10)
-                    .map(|cube_num| {
-                        command_spawn((
-                            SceneRoot(handle.clone()),
-                            Transform::from_xyz(10.0, cube_num as f32, 3.0 * (cube_num - 5) as f32),
-                            Paintable,
-                        ))
-                    })
-                    .collect::<Vec<_>>()
-            },
-        ),
-        command_spawn((DirectionalLight::default(), PAINTABLE_LAYER)),
-    ))
+    Ok((asset_server_load_and(
+        GltfAssetLabel::Scene(0).from_asset(assets.cube.path().ok_or(GltfAssetNotStrongPath)?),
+        |handle| {
+            (0..10)
+                .map(|cube_num| {
+                    command_spawn((
+                        SceneRoot(handle.clone()),
+                        Transform::from_xyz(10.0, cube_num as f32, 3.0 * (cube_num - 5) as f32)
+                            .with_rotation(Quat::from_axis_angle(Vec3::Z, PI)),
+                        Paintable,
+                    ))
+                })
+                .collect::<Vec<_>>()
+        },
+    ),))
 }
 
 #[derive(Debug, Default, Clone, PartialEq, Eq, Resource, AssetCollection)]
