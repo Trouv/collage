2d4003b532887e505422251ba72f1a47d542d504
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-02-09 23:33:05 -0700

    feat: begin writing OctahedronWithUvs type

--- src/clear_skies/paint_skies/triangle_with_uvs.rs
+++ src/clear_skies/paint_skies/triangle_with_uvs.rs
@@ -33,3 +33,50 @@ impl TriangleWithUvs {
         (centroid, TriangleWithUvs { triangle, ..self })
     }
 }
+
+/// An octahedron type, built by creating 6 triangles between two triangular faces.
+pub struct OctahedronWithUvs {
+    /// The near face.
+    pub near_face: TriangleWithUvs,
+    /// The far face.
+    pub far_face: TriangleWithUvs,
+}
+
+impl OctahedronWithUvs {
+    /// Returns the centroid of the octahedron and a new `OctahedronWithUvs` whose vertices are
+    /// relative to that centroid.,
+    pub fn centered(self) -> (Vec3, OctahedronWithUvs) {
+        let near_centroid = self.near_face.triangle.centroid();
+        let far_centroid = self.far_face.triangle.centroid();
+
+        let centroid = (near_centroid + far_centroid) / 2.0;
+
+        let near_triangle = Triangle3d {
+            vertices: self
+                .near_face
+                .triangle
+                .vertices
+                .map(|vertex| vertex - centroid),
+        };
+        let far_triangle = Triangle3d {
+            vertices: self
+                .far_face
+                .triangle
+                .vertices
+                .map(|vertex| vertex - centroid),
+        };
+
+        let centered = OctahedronWithUvs {
+            near_face: TriangleWithUvs {
+                triangle: near_triangle,
+                ..self.near_face
+            },
+            far_face: TriangleWithUvs {
+                triangle: far_triangle,
+                ..self.far_face
+            },
+        };
+
+        (centroid, centered)
+    }
+}
