c8222b73f66b80edce72478cb194d61c242a81b1
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-02-18 22:08:38 -0700

    feat: use concrete type for control_spherical_coords system

--- src/clear_skies/paint_skies/control_spherical_coords.rs
+++ src/clear_skies/paint_skies/control_spherical_coords.rs
@@ -11,16 +11,21 @@ use crate::clear_skies::paint_skies::spherical_coords::{
     SphericalCoordsBounds,
 };
 
-pub fn control_spherical_coords(settings: Res<PaintSkiesSettings>) -> impl Effect + use<> {
+pub fn control_spherical_coords(
+    settings: Res<PaintSkiesSettings>,
+) -> ComponentsSetFilteredWithQueryData<
+    (LookAtSphericalCoords,),
+    (
+        &'static ActionState<PaintSkiesAction>,
+        &'static SphericalCoordsBounds,
+    ),
+    With<PaintSkiesCamera>,
+> {
     let rotate_sensitivity = settings.rotate_sensitivity;
 
-    components_set_filtered_with_query_data::<
-        _,
-        _,
-        (&ActionState<PaintSkiesAction>, &SphericalCoordsBounds),
-        With<PaintSkiesCamera>,
-    >(
-        move |(spherical_coords,): (LookAtSphericalCoords,), (action_state, bounds)| {
+    components_set_filtered_with_query_data(
+        move |(spherical_coords,): (LookAtSphericalCoords,),
+              (action_state, bounds): (&ActionState<PaintSkiesAction>, &SphericalCoordsBounds)| {
             let rotate_by = action_state.clamped_axis_pair(&PaintSkiesAction::Rotate);
 
             let phi = (spherical_coords.phi + (rotate_by.y * rotate_sensitivity))
