c917a6de5c186da55481a0ae292e967c7867e21c
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-02-21 12:42:36 -0700

    feat: write main systems of PaintLayerHistoryPlugin

--- src/clear_skies/paint_skies/paint_layer_history.rs
+++ src/clear_skies/paint_skies/paint_layer_history.rs
@@ -3,7 +3,7 @@ use std::marker::PhantomData;
 use bevy::prelude::*;
 use bevy_pipe_affect::prelude::*;
 
-use crate::clear_skies::paint_skies::paint_meshes::LayerIndex;
+use crate::clear_skies::paint_skies::paint_meshes::{LayerIndex, ReadyToPaint};
 
 /// Plugin that tracks the history of a component at previous paint layers.
 #[derive(Copy, Clone, PartialEq, Eq, Debug, Default)]
@@ -11,13 +11,39 @@ pub struct PaintLayerHistoryPlugin<C>(PhantomData<C>);
 
 impl<C> Plugin for PaintLayerHistoryPlugin<C>
 where
-    C: Send + Sync + 'static,
+    C: Component + Clone + Send + Sync + 'static,
 {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_systems(
+            Update,
+            record_history::<C>
+                .pipe(affect)
+                .run_if(on_message::<ReadyToPaint>),
+        );
+    }
 }
 
 #[derive(Clone, PartialEq, Eq, Debug, Default, Component)]
 pub struct PaintableHistory<C> {
     history: Vec<C>,
     initial_layer: LayerIndex,
+}
+
+impl<C> PaintableHistory<C> {
+    fn new_with_initial_layer(initial_layer: LayerIndex) -> Self {
+        PaintableHistory {
+            initial_layer,
+            history: vec![],
+        }
+    }
+}
+
+fn record_history<C>() -> ComponentsSetWithQueryData<(PaintableHistory<C>,), &'static C>
+where
+    C: Component + Clone,
+{
+    components_set_with_query_data(|(mut history,): (PaintableHistory<C>,), c: &C| {
+        history.history.push(c.clone());
+        (history,)
+    })
 }
