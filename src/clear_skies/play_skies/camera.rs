de4f205fa3968ff1e7b488f21688d7f5e198bade
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-03-02 21:54:04 -0700

    feat: move more components to be required by PlaySkiesCamera instead of explicitly spawned and make return type concrete

--- src/clear_skies/play_skies/camera.rs
+++ src/clear_skies/play_skies/camera.rs
@@ -1,4 +1,5 @@
 use bevy::camera::RenderTarget;
+use bevy::camera::visibility::RenderLayers;
 use bevy::prelude::*;
 use bevy_pipe_affect::prelude::*;
 
@@ -6,14 +7,14 @@ use crate::clear_skies::camera::ClearSkiesRenderTarget;
 use crate::clear_skies::render_layers::PAINTED_LAYER;
 
 #[derive(Default, Debug, PartialEq, Eq, Copy, Clone, Hash, Reflect, Component)]
-#[require(Name = "PlaySkiesCamera")]
+#[require(Name = "PlaySkiesCamera", Camera3d, RenderLayers = PAINTED_LAYER)]
 pub struct PlaySkiesCamera;
 
-pub fn spawn_camera(render_target: Res<ClearSkiesRenderTarget>) -> impl Effect + use<> {
+pub fn spawn_camera(
+    render_target: Res<ClearSkiesRenderTarget>,
+) -> CommandSpawn<(PlaySkiesCamera, Camera, RenderTarget)> {
     command_spawn((
-        Camera3d::default(),
         PlaySkiesCamera,
-        PAINTED_LAYER,
         Camera {
             order: 1,
             clear_color: ClearColorConfig::Custom(Color::srgb(0.0, 0.4, 1.0)),
