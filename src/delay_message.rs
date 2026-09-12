8b8eefc427c2ddcc743f557912fef35dce25b49e
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-09-11 21:33:18 -0600

    feat: make DELAY_FRAMES a constant on DelayMessagePlugin

--- src/delay_message.rs
+++ src/delay_message.rs
@@ -5,84 +5,75 @@ use bevy::ecs::message::Message;
 use bevy::prelude::*;
 use bevy_pipe_affect::prelude::*;
 
-pub struct DelayMessagePlugin<M: Message> {
-    pub delay_frames: u32,
+#[derive(Copy, Clone, PartialEq, Eq, Default, Debug)]
+pub struct DelayMessagePlugin<M: Message, const DELAY_FRAMES: u32> {
     phantom: PhantomData<M>,
 }
 
-impl<M: Message> DelayMessagePlugin<M> {
-    pub fn new(delay_frames: u32) -> Self {
-        DelayMessagePlugin {
-            delay_frames,
-            phantom: PhantomData,
-        }
-    }
-}
-
-impl<M: Message + Clone> Plugin for DelayMessagePlugin<M> {
+impl<M: Message + Clone, const DELAY_FRAMES: u32> Plugin for DelayMessagePlugin<M, DELAY_FRAMES> {
     fn build(&self, app: &mut App) {
-        app.add_message::<DelayedMessage<M>>()
-            .insert_resource(OldMessageQueue::<M>(vec![]))
+        app.add_message::<DelayedMessage<M, DELAY_FRAMES>>()
+            .insert_resource(OldMessageQueue::<M, DELAY_FRAMES>(vec![]))
             .add_systems(
                 Last,
-                record_messages::<M>.pipe(affect).before(update_frame_count),
+                record_messages::<M, DELAY_FRAMES>
+                    .pipe(affect)
+                    .before(update_frame_count),
             )
             .add_systems(
                 First,
-                write_delayed_message_system::<M>(self.delay_frames).pipe(affect),
+                write_delayed_message_system::<M, DELAY_FRAMES>.pipe(affect),
             );
     }
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, Default, Debug, Message, Deref, DerefMut)]
-pub struct DelayedMessage<M: Message>(pub M);
+pub struct DelayedMessage<M: Message, const DELAY_FRAMES: u32>(pub M);
 
 #[derive(Clone, PartialEq, Eq, Default, Debug, Resource, Deref, DerefMut)]
-struct OldMessageQueue<M: Message>(Vec<(u32, M)>);
+struct OldMessageQueue<M: Message, const DELAY_FRAMES: u32>(Vec<(u32, M)>);
 
-fn write_delayed_message_system<M: Message + Clone>(
-    delay_frames: u32,
-) -> impl Fn(
-    Res<OldMessageQueue<M>>,
-    Res<FrameCount>,
+fn write_delayed_message_system<M: Message + Clone, const DELAY_FRAMES: u32>(
+    old_message_queue: Res<OldMessageQueue<M, DELAY_FRAMES>>,
+    frame_count: Res<FrameCount>,
 ) -> (
-    Vec<MessageWrite<DelayedMessage<M>>>,
-    ResSet<OldMessageQueue<M>>,
+    Vec<MessageWrite<DelayedMessage<M, DELAY_FRAMES>>>,
+    ResSet<OldMessageQueue<M, DELAY_FRAMES>>,
 ) {
-    move |old_message_queue, frame_count| {
-        let (messages_to_write, new_message_queue): (Vec<_>, Vec<_>) = old_message_queue
-            .0
-            .clone()
-            .into_iter()
-            .partition(|(original_frame, _)| {
-                original_frame.saturating_add(delay_frames) < frame_count.0
-            });
+    let (messages_to_write, new_message_queue): (Vec<_>, Vec<_>) = old_message_queue
+        .0
+        .clone()
+        .into_iter()
+        .partition(|(original_frame, _)| {
+            original_frame.saturating_add(DELAY_FRAMES) < frame_count.0
+        });
 
-        (
-            messages_to_write
-                .into_iter()
-                .map(|(_, message)| message_write(DelayedMessage(message)))
-                .collect(),
-            res_set(OldMessageQueue(new_message_queue)),
-        )
-    }
+    (
+        messages_to_write
+            .into_iter()
+            .map(|(_, message)| message_write(DelayedMessage(message)))
+            .collect(),
+        res_set(OldMessageQueue(new_message_queue)),
+    )
 }
 
-fn record_messages<M: Message + Clone>(
+fn record_messages<M: Message + Clone, const DELAY_FRAMES: u32>(
     frame_count: Res<FrameCount>,
-) -> MessagesReadAnd<M, ResSetWith<OldMessageQueue<M>>> {
+) -> MessagesReadAnd<M, ResSetWith<OldMessageQueue<M, DELAY_FRAMES>>> {
     let frame_count = frame_count.0;
     messages_read_and(move |m: &M| {
         let m = m.clone();
-        res_set_with(move |old_message_queue: &OldMessageQueue<M>| {
-            OldMessageQueue(
-                old_message_queue
-                    .0
-                    .clone()
-                    .into_iter()
-                    .chain(std::iter::once((frame_count, m)))
-                    .collect(),
-            )
-        })
+        res_set_with(
+            move |old_message_queue: &OldMessageQueue<M, DELAY_FRAMES>| {
+                OldMessageQueue(
+                    old_message_queue
+                        .0
+                        .clone()
+                        .into_iter()
+                        .chain(std::iter::once((frame_count, m)))
+                        .collect(),
+                )
+            },
+        )
     })
 }
